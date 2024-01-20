6b061b78a57b3b987fa256b43cc606dd0d03c9b7
deepalikanade4 <131874829+deepalikanade4@users.noreply.github.com>
2024-01-20 17:15:20 +0530

    Update models.py

--- pmsapp/models.py
+++ pmsapp/models.py
@@ -3,6 +3,26 @@ from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
 
 
+# # Create your models here.
+
+# class UserModel(AbstractUser):
+#     # user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+#     # username=models.CharField(max_length=70,unique=True)
+#     last_name=None
+#     first_name=None
+
+#     designation=models.CharField(max_length=250)
+#     email=models.EmailField(max_length=350)
+#     # password=models.CharField(max_length=250)
+#     objects = CustomUserManager()
+#     # USERNAME_FIELD=username
+#     REQUIRED_FIELDS=[]
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import CustomUserManager
+
+
 # Create your models here.
 
 class UserModel(AbstractUser):
@@ -19,4 +39,5 @@ class UserModel(AbstractUser):
     # USERNAME_FIELD=username
     REQUIRED_FIELDS=[]
 
+    
     
