c4d008e9d264ef3f2a6aad1535d8033c3823e9e2
deepalikanade4 <131874829+deepalikanade4@users.noreply.github.com>
2024-01-20 17:13:17 +0530

    Update forms.py
    form changes 3rd commit

--- pmsapp/forms.py
+++ pmsapp/forms.py
@@ -6,20 +6,20 @@ from .models import UserModel
 # from django.contrib.auth import get_user_model
 # User = get_user_model()
 
-class UserRegistrationform(forms.ModelForm):
+class UserRegistrationform(UserCreationForm):
     
-    # password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    # password2=forms.CharField(label='Confirm Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password2=forms.CharField(label='Confirm Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
     # designation=forms.CharField(max_length=100)
     # email=forms.EmailField(max_length=200)
     class Meta:
         model=UserModel
-        fields=['username','password','designation','email']
+        fields=['username','designation','email']
         widget={
             'username':forms.TextInput(attrs={'class':'form-control'}),
             'designation':forms.TextInput(attrs={'class':'form-control'}),
-            'email':forms.EmailInput(attrs={'class':'form-control'})
-
+            'email':forms.EmailInput(attrs={'class':'form-control'}),
+            #'password1' : forms.PasswordInput(attrs={'class':'form-control'
         }
 
 
